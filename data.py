6851c04e4fd04b58885cf54443a3e973df6c0530
Mond <herecomesthemoon@protonmail.com>
2022-12-01 21:15:21 +0100

    Initial commit

---
+++ data.py
@@ -0,0 +1,29 @@
+import requests
+from datetime import date
+from configparser import ConfigParser
+
+SESSION_ID = ConfigParser().read('config.ini')['config']['SESSION_ID']
+YEAR = 2022
+INPUTS = "./data/"
+
+
+def load(day: int) -> str:
+    assert isinstance(day, int)
+    assert 0 < day < 26
+    s = f"https://adventofcode.com/{YEAR}/day/{day}/input"
+    cookies = {"session": SESSION_ID}
+
+    resp: requests.Response = requests.get(s, cookies=cookies)
+
+    return resp.text
+
+
+def save(path: str, data: str):
+    with open(path, 'w') as file:
+        file.write(data)
+
+
+if __name__ == '__main__':
+    day = date.today().day
+    input = load(day)
+    save(f"./data/{day}.txt", input)
