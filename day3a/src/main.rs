6b08cee291718025fd730ffa08ac2d96b98f546f
Mond <herecomesthemoon@protonmail.com>
2022-12-04 21:23:55 +0100

    Cleaning up a bit

--- day3a/src/main.rs
+++ day3a/src/main.rs
@@ -2,21 +2,19 @@ use std::collections::HashSet;
 
 fn main() {
     let mut sum_of_priorities = 0;
-    let data = include_str!("../../data/3.txt");
-    // let data = "vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\nttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw";
-    let data = data.lines().map(|row| row.split_at(row.len() / 2));
+    let data = include_str!("../../data/3.txt").lines().map(|row| row.as_bytes().split_at(row.len() / 2));
     for (head, tail) in data {
-        let a: HashSet<char> = HashSet::from_iter(head.chars());
-        let b = HashSet::from_iter(tail.chars());
-        let mut res = HashSet::intersection(&a, &b);
-        let item = *res.next().unwrap() as u8;
-        println!("{}", sum_of_priorities);
-        println!("{}", item as char);
-        sum_of_priorities += match item {
-            b'a'..=b'z' => { 1 + item - b'a' },
-            b'A'..=b'Z' => { 27 + item - b'A' },
-            _ => { unreachable!() },
-        } as usize
+        let a: HashSet<&u8> = HashSet::from_iter(head);
+        for x in tail {
+            if a.contains(x) {
+                sum_of_priorities += match x {
+                    b'a'..=b'z' => { 1 + x - b'a' },
+                    b'A'..=b'Z' => { 27 + x - b'A' },
+                    _ => { unreachable!() },
+                } as usize;
+                break;
+            }
+        }
     }
     println!("{}", sum_of_priorities);
 }
--- day3b/src/main.rs
+++ day3b/src/main.rs
@@ -2,19 +2,17 @@ use std::collections::HashSet;
 
 fn main() {
     let mut sum_of_priorities = 0;
-    let data = include_str!("../../data/3.txt");
-    // let data = "vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\nttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw";
-    let data: Vec<_> = data.lines().collect();
+    let data: Vec<_> = include_str!("../../data/3.txt").lines().collect();
     for k in (0..data.len()).step_by(3) {
-        let a: HashSet<char> = HashSet::from_iter(data[k].chars());
-        let b: HashSet<char> = HashSet::from_iter(data[k+1].chars());
-        let c: HashSet<char> = HashSet::from_iter(data[k+2].chars());
+        let a: HashSet<&u8> = HashSet::from_iter(data[k].as_bytes());
+        let b: HashSet<&u8> = HashSet::from_iter(data[k+1].as_bytes());
+        let c: HashSet<&u8> = HashSet::from_iter(data[k+2].as_bytes());
 
         for &x in a.intersection(&b) {
             if c.contains(&x) {
-                sum_of_priorities += match x as u8 {
-                    b'a'..=b'z' => 1 + x as u8 - b'a',
-                    b'A'..=b'Z' => 27 + x as u8 - b'A',
+                sum_of_priorities += match x {
+                    b'a'..=b'z' => 1 + x - b'a',
+                    b'A'..=b'Z' => 27 + x - b'A',
                     _ => {
                         unreachable!()
                     }
