05384f0a6de315aaf46d75b0f9017bf72fbdcaf8
Mond <herecomesthemoon@protonmail.com>
2022-12-06 12:51:10 +0100

    Multithreaded for speed

--- goday6b/main.go
+++ goday6b/main.go
@@ -4,40 +4,68 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"runtime"
 )
 
-const FILE = "./../data/6.txt"
+const FILE = "./../bigboy/6.txt"
 
 func main() {
-	data := read()
+	f, err := os.Open(FILE)
+	defer f.Close()
+	if err != nil {
+		panic(err)
+	}
+	// f := strings.NewReader("mjqjpqmgbljsphdztnvjfqwrcgsmlb")
+	solve(f, runtime.NumCPU() + 1)
+}
 
+func solve(input io.Reader, threads int) {
+	data, err := io.ReadAll(input)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("File read.")
+
+	chunk_size := len(data)/threads + 1
+	chans := make([]chan int, 0, threads)
+	for i := 0; i < threads; i++ {
+		ch := make(chan int)
+		chans = append(chans, ch)
+		go compute(data[i*chunk_size:(i+1)*chunk_size+14], ch)
+	}
+
+	for i := 0; i < threads; i++ {
+		fmt.Printf("Waiting for %d...\n", i)
+		result := <-chans[i]
+		fmt.Printf("Received %d\n", result)
+		if result == -1 {
+			continue
+		}
+		fmt.Printf("The first start-of-message marker is at position %d\n", i*chunk_size + result)
+		return
+	}
+
+}
+
+func compute(chunk []byte, ch chan int) {
 	in_window := map[byte]struct{}{}
 
 	i := 0
-	for j, v := range data {
+	for j, v := range chunk {
 		if _, ok := in_window[v]; ok {
-			for data[i] != v {
-				delete(in_window, data[i])
+			for chunk[i] != v {
+				delete(in_window, chunk[i])
 				i++
 			}
 			i++
 		}
-		in_window[v] = struct{}{};
-		if j - i == 13 {
-			fmt.Printf("The first start-of-message marker is at position %d\n", j + 1)
+		in_window[v] = struct{}{}
+		if j-i == 13 {
+			ch <- j + 1
 			break
 		}
 	}
+	ch <- -1
 }
 
 
-func read() []byte {
-	f, err := os.Open(FILE)
-	defer f.Close()
-	if err != nil { panic(err) }
-	// f := strings.NewReader("mjqjpqmgbljsphdztnvjfqwrcgsmlb")
-
-	result, err := io.ReadAll(f)
-	if err != nil { panic(err) }
-	return result
-}
